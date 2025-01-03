0288f51ccc8f226f40180e1e0069781593a5688a
mane05 <mrutvik151@gmail.com>
2025-01-03 22:51:40 +0530

    Navbar Created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavbarMenu } from '../mockdata/data'
+import { CiSearch } from "react-icons/ci";
+import { PiShoppingCartThin } from "react-icons/pi";
+import { FaDumbbell } from "react-icons/fa";
+import { MdMenu } from "react-icons/md";
+import ResponsiveMenu from './ResponsiveMenu';
 
-function Navbar() {
+export const Navbar = () => {
+  const[open,setopen]=useState(false)
   return (
-    <div>Navbar
-    <div>
-      <ul>
-        <li>Home</li>
-        <li>AboutUs</li>
-        <li>ContactUs</li>
-        <li>Login</li>
-      </ul>
-    </div>
-    </div>
+    <>
+    <nav>
+     <div className='container flex justify-center items-center py-9'>
+      {/* Logo section */}
+      <div className='text-2xl  flex items-center gap-2 font-bold Uppercase'>
+       < FaDumbbell/>
+       <p>Coders</p>
+       <p className='text-secoundry'>Gym</p>
+      </div>
+      {/* Menu section */}
+      <div className='hidden md:block'>
+        <ul className='flex items-center gap-6 text-gray-600'>
+         {NavbarMenu.map((item)=>{
+          return(
+            <li key={item.id}><a href={item.link} className='inline-block py-1 px-3 hover:text-primary font-semibold'>{item.title}
+            </a>
+            </li>
+          )
+         })}
+        </ul>
+      </div>
+      {/* Icons section */}
+      <div className=' flex items-center gap-4'>
+        <button className='text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200'>
+          <CiSearch />
+        </button>
+        <button className='text-2xl hover:bg-primary hover:text-white rounded-full p-2 duration-200'>
+          <PiShoppingCartThin />
+        </button> 
+        <button className='hover:bg-primary text-primary  font-semibold hover:text-white rounded-md border-2 border-primary
+         px-6 py-2 duration-200 hidden md:block 
+         '>Login</button>
+      </div> 
+     </div>
+     {/* Mobile Humburger Menu section */}
+     <div className='md:hidden' onCanPlay={()=>{
+      setopen(!open)
+     }}>
+      <MdMenu className='text-4xl'/>
+     </div>
+     {/* Mobile Sidebar section */}
+     <ResponsiveMenu open={open}/>
+    
+   </nav>
+    </>
+   
   )
 }
-
-export default Navbar   
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors:{
+      primary:"#ff8901",
+      secoundry:"#fb923c"
+
+    },
+    extend: {
+      container:{
+        center:true,
+        padding:{
+          default:'1rem',
+          sm:'2rem',
+          lg:'4rem',
+          xl:'5rem',
+          '2xl':'6rem'
+
+        }
+      }
+    },
   },
   plugins: [],
 }
