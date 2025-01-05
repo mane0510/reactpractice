99156cfcb3075f50ffa61b32055a5dbe1e005e60
mane05 <mrutvik151@gmail.com>
2025-01-05 15:48:13 +0530

    Switch Approch  in UseState Hook Commit

--- src/components/UseReducerHook/UseReducerHook.js
+++ src/components/UseReducerHook/UseReducerHook.js
@@ -1,39 +1,37 @@
-import React, { useReducer } from 'react'
+import React, { useReducer } from 'react';
 
 export const UseReducerHook = () => {
+  const initialState = {
+    count: 0,
+    inc :2,
+    dec : 2,
+  };
 
-
-
-    const reducer=(state,action)=>{
-        console.log(state,action)
-         if(action.type==="INCREMENT"){
-           return state + 1
-
-       
-            }
-        if(action.type==="DECREMENT"){
-          return state - 1
-
-       }
-    if(action.type==="RESET"){
-           return (state = 0)
+  const reducer = (state, action) => {
+    console.log(state, action);
+    switch (action.type) {
+      case 'INCREMENT':
+        return { 
+            ...state,
+            count: state.count + 1 };
+      case 'DECREMENT':
+        return {...state, count: state.count - 1 };
+      case 'RESET':
+        return { ...state,count: 0 };
+      default:
+        return state;
     }
-       
-        
-    }
-    const[count,dispatch]=useReducer(reducer,0)
-    console.log(useReducer());
+  };
 
-   
+  const [state, dispatch] = useReducer(reducer, initialState); 
   return (
     <>
-    <div className='p-4 h-lvh flex flex-col justify-center items-center'>
-        <h1>{count}</h1>
-        <button onClick={()=>dispatch({type:"INCREMENT"})}>Increment</button>
-        <button onClick={()=>dispatch({type:"DECREMENT"})}>Decrement</button>
-        <button onClick={()=>dispatch({type:"RESET"})}>Reset</button>
-    </div>
+      <div className="p-4 h-lvh flex flex-col justify-center items-center">
+        <h1>{state.count}</h1> 
+        <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
+        <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
+        <button onClick={() => dispatch({ type: 'RESET' })}>Reset</button>
+      </div>
     </>
-    
-  )
-}
+  );
+};
